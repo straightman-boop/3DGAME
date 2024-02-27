36b5b8c2a0e8ac2506f5dc534c88f67560dc2e9c
jsp5a01 <jeremyjosh.abad.21@usjr.edu.ph>
2024-02-27 16:26:57 +0800

    MainMenu - FIXED

---
+++ Assets/UI_Settings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Settings : MonoBehaviour
+{
+    public void CloseSettings()
+    {
+        gameObject.SetActive(false);
+    }
+}

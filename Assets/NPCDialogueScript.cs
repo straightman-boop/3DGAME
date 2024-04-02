2bb356e58a0fd2700a78629f19deae9b9da6dcb9
gotme_Krackin <jeremyjosh.abad.21@usjr.edu.ph>
2024-04-03 01:53:07 +0800

    NPC Additions

--- Assets/NPCDialogueScript.cs
+++ Assets/NPCDialogueScript.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class NPCDialogueScript : MonoBehaviour
 {
+    bool onceOnly = false;
+    public GameObject NPCDialogueSystem;
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+
+
+    int pageTurn = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NPCDialogueSystem.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NPCDialogueSystem.SetActive(false);
+    }
+
+    public void switchPage()
+    {
+        if (pageTurn == 1)
+        {
+            p1.SetActive(false);
+            p2.SetActive(true);
+            pageTurn = 2;
+        }
+
+        else if (pageTurn == 2)
+        {
+            p2.SetActive(false);
+            p3.SetActive(true);
+            pageTurn = 3;
+        }
+
+        else if (pageTurn == 3)
+        {
+            NPCDialogueSystem.SetActive(false);
+        }
     }
 }
